5313a2a7a42f366823cc4f1da4de370d8983693b
andre <andre@DESKTOP-IROGES6>
2020-12-16 22:13:51 -0600

    modificacion

---
+++ src/Infoproducto.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JOptionPane;
+
+
+/*public class Infoproducto {
+    private int marca; 
+    private int talla; 
+    public int getmarca() {
+        return marca;
+    }
+    public void setmarca(int marca) {
+        this.marca = marca;
+}
+    public int gettalla() {
+        return talla;
+    }
+    public void setNom(int talla) {
+        this.talla = talla;*/
+       //}
+ //public void Info(){
+ //marca= Integer.parseInt(JOptionPane.showInputDialog ("Digite la marca deseada:"));
+ 
+// talla= Integer.parseInt(JOptionPane.showInputDialog  ("Digite la talla deseada:"));
+ 
+ 
+ }   
+}
